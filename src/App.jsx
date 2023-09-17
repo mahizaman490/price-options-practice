b808556c55c401b4aef31de45247efbb41d34af6
mahizaman490 <mahizaman490@gmail.com>
2023-09-17 20:22:40 +0600

    first commit with creating  navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from './components/NavBar/NavBar'
+import './App.css'
+// import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+ 
+
+  return (
+    <>
+    {/* <DaisyNav></DaisyNav> */}
+    <NavBar></NavBar>
+      <h1 className='text-5xl bg-rose-500'>Vite + React</h1>
+
+    </>
+  )
+}
+
+export default App
